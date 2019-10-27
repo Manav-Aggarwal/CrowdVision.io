862036b7f08015c63aa901d56fe4c352dfb46581
Manav Aggarwal <manavaggarwal1234@gmail.com>
2019-10-27 07:06:33 -0700

    Added requirements file

--- webcam_data.py
+++ webcam_data.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-import urllib3
 from is_military import predict
 from is_military_resnet import *
 from send_data import ping_server, get_location
@@ -10,9 +9,8 @@ def cv_to_pil_img(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return Image.fromarray(img)
 
-
 cv2.namedWindow("webcam")
-vc = cv2.VideoCapture(1)
+vc = cv2.VideoCapture(0)
 
 net = cv2.dnn.readNetFromCaffe('models/bvlc_googlenet.prototxt', 'models/bvlc_googlenet.caffemodel')
 rows = open('synset_words.txt').read().strip().split("\n")
@@ -26,7 +24,6 @@ else:
     rval = False
 
 begin = time.time()
-http = urllib3.PoolManager()
 path = 'locations.txt'
 while rval:
     print("step:", (time.time() - begin))
@@ -42,6 +39,7 @@ while rval:
     if wasFound:
         with open(path, 'a') as f:
             f.write(str(get_location()) + "\n")
+        break
         time.sleep(3)
     key = cv2.waitKey(20)
     if key == 27:
