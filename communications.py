c2d633c0332ef09a8d1e4bfa1dc2260bd0fac419
Manav Aggarwal <manavaggarwal1234@gmail.com>
2019-10-27 06:31:38 -0700

    Updated files

--- communications.py
+++ communications.py
@@ -6,17 +6,18 @@ path = 'locations.txt'
 with open(path, 'r') as f:
 	lastSeen = f.readlines()[-1]
 
-vibration = "y"
+vibration = "n"
 with serial.Serial('/dev/cu.usbmodem14301',9600,timeout=10) as ser:
 	while True:
-		print(vibration)
-		# with open(path, 'r') as f:
-		# 	lines = f.readlines()
-		# 	if lines and lastSeen != lines[-1]:
-		# 		lastSeen = lines[-1]
-		# 		vibration = "y"
+		#print(vibration)
+		with open(path, 'r') as f:
+			lines = f.readlines()
+			if lines and lastSeen != lines[-1]:
+				lastSeen = lines[-1]
+				vibration = "y"
 		if vibration == "y":
 			ser.write(bytes('YES\n','utf-8'))
 		elif vibration == "n":
 			ser.write(bytes('NO\n','utf-8'))
-		#vibration = "n"
+		time.sleep(1)
+		vibration = "n"
